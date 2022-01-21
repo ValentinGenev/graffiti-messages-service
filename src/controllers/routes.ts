1953383584cbc03afb85c86fa6bfaa47486c36a8
valentingenev <valentin_genev@abv.bg>
2022-01-21 18:17:07 +0200

    [+] very basic documentation of the get requests;

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,11 +1,13 @@
-import { RestServer } from "../lib/rest"
-import { healthCheck } from "./health-check"
-import { postMessage } from "./post"
-import { getMessages, getMessage } from "./get"
+import { RestServer } from '../lib/rest'
+import { documentation } from './documentation'
+import { healthCheck } from './health-check'
+import { postMessage } from './post'
+import { getMessages, getMessage } from './get'
 
 export function setRoutes(rest: RestServer): void {
     const server = rest.getServer()
 
+    server.get('/', documentation)
     server.get('/health-check', healthCheck)
     server.post('/message', postMessage)
     server.get('/messages', getMessages)
