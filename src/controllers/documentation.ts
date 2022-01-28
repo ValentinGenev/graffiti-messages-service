ddaf326424fd2d8d90df23c50e639e984a583c6f
valentingenev <valentin_genev@abv.bg>
2022-01-28 12:57:19 +0200

    [=] documentation moved to public/;

--- src/controllers/documentation.ts
+++ src/controllers/documentation.ts
@@ -2,7 +2,7 @@ import path from 'path'
 
 export function documentation(request: Record<string, any>, response: Record<string, any>): void {
     try {
-        response.sendFile(path.join(__dirname, 'documentation.html'))
+        response.sendFile(path.join(__dirname, '../public/documentation.html'))
     }
     catch (error) {
         // TODO: set different response codes
