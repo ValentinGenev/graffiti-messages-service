2255756d3507615354d1845d4257272faf50393b
valentingenev <valentin_genev@abv.bg>
2022-02-13 11:33:26 +0200

    [=] messages api path changed;

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,17 +1,23 @@
+import dotenv from 'dotenv'
 import path from 'path'
 import { RestServer } from '../lib/rest'
 import { postMessage } from './post'
 import { getMessages, getMessage } from './get'
 
+dotenv.config()
+
+const REST_PATH = process.env.REST_PATH
+
 export function setRoutes(rest: RestServer): void {
     const server = rest.getServer()
 
-    server.get('/', documentation)
-    server.get('/health-check', healthCheck)
-    server.post('/message', postMessage)
-    server.get('/messages', getMessages)
-    server.get('/messages/:pageIndex', getMessages)
-    server.get('/last-message/:posterId', getMessage)
+    server.get(`${REST_PATH}/`, documentation)
+    server.get(`${REST_PATH}/health-check`, healthCheck)
+    server.get(`${REST_PATH}/all`, getMessages)
+    server.get(`${REST_PATH}/all/:pageIndex`, getMessages)
+    server.get(`${REST_PATH}/last/:posterId`, getMessage)
+    
+    server.post(`${REST_PATH}/new`, postMessage)
 }
 
 export function healthCheck(_request: Record<string, any>, response: Record<string, any>): void {
