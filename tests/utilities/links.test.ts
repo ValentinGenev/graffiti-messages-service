d1a23b3c1dcdef227c893a40c322fe37996cb2db
valentingenev <valentin_genev@abv.bg>
2022-10-09 14:44:14 +0300

    [=] query added to the pagination._links;

--- tests/utilities/links.test.ts
+++ tests/utilities/links.test.ts
@@ -3,7 +3,7 @@ import { database } from '../../src'
 import { addSelfLink, addTagsLinks, addPaginationLinks }
     from '../../src/utilities/links'
 import { message, messageId, pagination, paginationWithNext,
-    paginationWithPrevious, tag } from '../mock/data'
+    paginationWithPrevious, tag, tagFilter } from '../mock/data'
 
 describe('Links functions tests:', () => {
     test('addSelfLink(messages)', () => {
@@ -25,21 +25,21 @@ describe('Links functions tests:', () => {
 
     test('addPaginationLinks(paginationWithNext)', () => {
         const { postsPerPage, nextPageIndex } = paginationWithNext
-        const result = addPaginationLinks(paginationWithNext)
+        const result = addPaginationLinks(paginationWithNext, tagFilter)
 
         expect(result.next?.name === 'Next page').toBeTruthy()
         expect(result.next?.href ===
-                `/messages/?pageIndex=${nextPageIndex}&postsPerPage=${postsPerPage}`)
+                `/messages/?pageIndex=${nextPageIndex}&postsPerPage=${postsPerPage}&tag=${tagFilter.tag}`)
             .toBeTruthy()
     })
 
     test('addPaginationLinks(paginationWithPrevious)', () => {
         const { postsPerPage, previousPageIndex } = paginationWithPrevious
-        const result = addPaginationLinks(paginationWithPrevious)
+        const result = addPaginationLinks(paginationWithPrevious, tagFilter)
 
         expect(result.prev?.name === 'Previous page').toBeTruthy()
         expect(result.prev?.href ===
-                `/messages/?pageIndex=${previousPageIndex}&postsPerPage=${postsPerPage}`)
+                `/messages/?pageIndex=${previousPageIndex}&postsPerPage=${postsPerPage}&tag=${tagFilter.tag}`)
             .toBeTruthy()
     })
 
